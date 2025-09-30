10c01ea603a8f56148fe72557cb7013d365d98a4
Matthew Schmitt <matt.schmitt@ent.security>
2025-09-30 16:23:26 -0700

    Renaming files

---
+++ variables.tf
@@ -0,0 +1,87 @@
+variable "name_prefix" {
+  description = "String to use as prefix on object names"
+  type        = string
+}
+
+variable "name_suffix" {
+  description = "String to append to object names. This is optional, so start with dash if using"
+  type        = string
+  default     = ""
+}
+
+variable "log_bucket_id" {
+  description = "ID of logging bucket to be targeted for S3 bucket logs"
+  type        = string
+}
+
+variable "log_bucket_target_object_key_format" {
+  description = "Map containing logging bucket target object key format configuration."
+  type        = any
+  default     = {}
+}
+
+variable "log_bucket_target_prefix" {
+  description = "The prefix for all log object keys. Define this varible to override the default."
+  type        = string
+  default     = ""
+}
+
+variable "account_arns" {
+  description = "Arns for accounts / roles in accounts which are given a role they are able to assume to access their state."
+  type        = list(string)
+  default     = []
+}
+
+variable "global_account_arns" {
+  description = "Arns for a account(s) / roles in account(s) that would be allowed access to all account states, for instance a global users account. Restrictions of which of that accounts users were able to access a given state would need to be further restricted inside of the global account(s) themselves."
+  type        = list(string)
+  default     = []
+}
+
+variable "input_tags" {
+  description = "Map of tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "block_public_acls" {
+  description = "Blocks public ACLs on the bucket."
+  type        = bool
+  default     = true
+}
+
+variable "block_public_policy" {
+  description = "Whether Amazon S3 should block public bucket policies for this bucket."
+  type        = bool
+  default     = true
+}
+
+variable "ignore_public_acls" {
+  description = "Whether Amazon S3 should ignore public ACLs for this bucket. Causes Amazon S3 to ignore public ACLs on this bucket and any objects that it contains."
+  type        = bool
+  default     = true
+}
+
+variable "restrict_public_buckets" {
+  description = "Whether Amazon S3 should restrict public bucket policies for this bucket. Enabling this setting does not affect the previously stored bucket policy, except that public and cross-account access within the public bucket policy, including non-public delegation to specific accounts, is blocked."
+  type        = bool
+  default     = true
+}
+
+variable "aws_s3_bucket_server_side_encryption_type" {
+  description = "Selection of the bucket encryption type"
+  type        = string
+  default     = "SSE_KMS"
+
+  validation {
+    condition = contains([
+      "AWS_DEFAULT",
+      "SSE_S3",
+      "SSE_KMS"
+      ],
+      var.aws_s3_bucket_server_side_encryption_type
+    )
+
+    error_message = "The valid values are AWS_DEFAULT, SSE_S3, SSE_KMS"
+  }
+}
